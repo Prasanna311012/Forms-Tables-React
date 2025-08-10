687a529149797742f5eb30d105effcecbb351b55
FSWD20017 PRASANNA S <prasannasasikumar3110@gmail.com>
2025-08-10 16:33:16 +0530

    update files

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react"
+import Form from "./components/Form"
+import Table from "./components/Table"
+function App() {
+  const initialFormData = {
+    name: "",
+    email: ""
+  }
+  const [formData, setFormData] = useState(initialFormData)
+  const [tableData, setTableData] = useState([])
+  const [editIndex, setEditIndex]=useState(null)
+
+  const handleFormDataChange = (key, value) => {
+
+    setFormData({
+      ...formData,
+      [key]: value,
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if(editIndex==null){
+      setTableData([
+      ...tableData, formData])
+    setFormData(initialFormData)
+    }
+    else{
+      tableData[editIndex]=formData
+      setFormData(initialFormData)
+      setTableData(tableData)
+      setEditIndex(null)
+    }
+  }
+
+  const handleEdit = (index) => {
+    console.log("edit clicked row", index)
+
+    const clickedItems=tableData[index]
+    setFormData(clickedItems)
+    setEditIndex(index)
+  }
+
+  const handleDelete=(index)=>{
+    tableData.splice(index,1)
+
+    setTableData([...tableData])
+  }
+
+  return (
+    <div className="p-6 mx-auto max-w-2xl">
+      <Form formData={formData}
+        handleFormDataChange={handleFormDataChange}
+        handleSubmit={handleSubmit}
+        editIndex={editIndex}
+      />
+      <Table tableData={tableData}
+       handleEdit={handleEdit}
+       handleDelete={handleDelete} />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Form.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Form=({formData,handleFormDataChange,handleSubmit,editIndex})=>{
+    return(
+        <>
+        <h1 className="text-2xl font-semibold mb-4 bg-green-200 rounded p-2">Forms and Tables</h1>
+        <form className="border rounded-lg p-4 bg-teal-100" onSubmit={handleSubmit}>
+            <input className="w-full p-2 border rounded mb-2" type="text" placeholder="Name" required
+            value={formData.name}
+            onChange={(e)=>{
+                const {value}=e.target;
+                handleFormDataChange("name",value);
+            }}
+            ></input>
+            <input className="w-full p-2 border rounded mb-2" type="email" placeholder="Email" required
+            value={formData.email}
+            onChange={(e)=>{
+                const {value}=e.target;
+                handleFormDataChange("email",value);
+            }}
+            ></input>
+            <button className="bg-blue-600 text-white px-4 py-2 rounded" type="submit">{editIndex == null?"Add" : "Update"}</button>
+        </form>
+        </>
+    )
+}
+export default Form;
